c20e70b8166d91eb2fb1881255fcdb982278363e
js <13923779+jaimesemp@users.noreply.github.com>
2025-01-15 14:18:50 +0100

    added ecommerce

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  title = 'Coca-Cola eCommerce';
+  products = [
+    { name: 'Botella de Cristal', size: '250ml', price: 1.50 },
+    { name: 'Botella de Plástico', size: '500ml', price: 2.00 },
+    { name: 'Botella de Plástico', size: '2L', price: 3.50 }
+  ];
+
+  addToCart(product: any) {
+    console.log('Añadido al carrito:', product);
+    // Aquí podrías implementar lógica para añadir al carrito
+  }
 
 }
